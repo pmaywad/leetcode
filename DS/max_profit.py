cd600863113fe1f1bbf26bc40532128cd97c07a5
Poonam Maywad <55203904+pmaywad@users.noreply.github.com>
2021-09-28 12:44:33 -0700

    Create max_profit.py

---
+++ DS/max_profit.py
@@ -0,0 +1,20 @@
+"""
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+"""
+
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        max_profit, min_price = 0, float('inf')
+        for price in prices:
+            min_price = min(min_price, price)
+            profit = price - min_price
+            max_profit = max(max_profit, profit)
+        return max_profit
