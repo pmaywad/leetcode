480519d137114d23d8597c84908e1a216780af1f
Poonam Maywad <55203904+pmaywad@users.noreply.github.com>
2021-09-28 11:59:06 -0700

    Update intersect_array.py

--- DS/intersect_array.py
+++ DS/intersect_array.py
@@ -1,7 +1,6 @@
 '''
 Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
 '''
-
 class Solution(object):
     def intersect(self, nums1, nums2):
         """
@@ -9,30 +8,17 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: List[int]
         """
-        freq_1 = {}
-        freq_2 = {}
-        res = []
-        for i in range(len(nums1)):
-            if nums1[i] in freq_1:
-                freq_1[nums1[i]]+=1
+        _dict={}
+        res=[]
+        for i in nums1:
+            if i not in _dict:
+                _dict[i]=1
             else:
-                freq_1[nums1[i]] = 1
-                
-        for i in range(len(nums2)):
-            if nums2[i] in freq_2:
-                freq_2[nums2[i]]+=1
-            else:
-                freq_2[nums2[i]] = 1
-                
-        for i in freq_1:
-            print(i)
-            if i in freq_2 and freq_1[i] <= freq_2[i]:
-                freq_2[i] -= freq_1[i]
-                for j in range(freq_1[i]):
-                    res.append(i)
-            elif i in freq_2 and freq_1[i] > freq_2[i]:
-                freq_1[i] -= freq_2[i]
-                for j in range(freq_2[i]):
-                    res.append(i)
-                
+                _dict[i]+=1
+        for i in nums2:
+            if i in _dict:
+                res.append(i)
+                _dict[i]-=1
+                if _dict[i]==0:
+                    _dict.pop(i)
         return res
