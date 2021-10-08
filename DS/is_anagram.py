d8273ad0d49bcfa2bf4e67083461345fb292ab10
Poonam Maywad <55203904+pmaywad@users.noreply.github.com>
2021-10-08 08:36:03 -0700

    Update is_anagram.py

--- DS/is_anagram.py
+++ DS/is_anagram.py
@@ -1,3 +1,8 @@
+"""
+242. Valid Anagram
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+"""
+
 class Solution(object):
     def isAnagram(self, s, t):
         """
