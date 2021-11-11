3b81cf0eafef7171e5e26a12c7ace202fd4deca2
Poonam Maywad <55203904+pmaywad@users.noreply.github.com>
2021-11-11 11:23:37 -0700

    Create iterative_reverseLL.py

---
+++ DS & Algo - Level-up/Linked List/iterative_reverseLL.py
@@ -0,0 +1,28 @@
+"""
+206. Reverse Linked List
+"""
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if head is None or head.next is None:
+            return head
+        
+        current = head
+        prev = None
+        while current is not None:
+            temp = current.next
+            current.next = prev
+            prev = current
+            current = temp
+            
+        return prev
